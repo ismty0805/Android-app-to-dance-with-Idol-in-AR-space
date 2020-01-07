4f8af9d487e8f95e907fd703ac3c3878ed0116d0
KwonHyungGeun <gene63@naver.com>
2020-01-08 06:26:35 +0900

    unity ar app

---
+++ videopractice/Assets/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Recorder;
+
+public class UIManager : MonoBehaviour
+{
+    public RecordManager recordManager;
+    public void StartVid(){
+        recordManager.StartRecord();
+    }
+    public void StopVid(){
+        recordManager.StopRecord();
+    }
+}
